a619d0d309eb479bedca2bbbead0f31a30365941
BaGorK <edmealemkassahun@gmail.com>
2023-12-22 03:34:41 -0800

    05-dropdown -- Building a Nice dropdown

---
+++ 05-dropdown/script.js
@@ -0,0 +1,7 @@
+// const anchor = document.querySelector('.nav__link-anchor--3');
+// anchor.addEventListener('mouseover', function (e) {
+// //   console.log(e.target);
+//   // DOM TRAVERSING
+//   e.target.closest('.nav__link').querySelector('.link__dropdown').style.display = 'block';
+// });
+// anchor.addEventListener('mouseout', function (e) {});
